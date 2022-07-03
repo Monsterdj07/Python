bacf263dd927b22cfe3f89fe3cbc4a8cb4ec54a7
Dheeraj Mourya <87568621+Monsterdj07@users.noreply.github.com>
2022-07-04 05:16:30 +0530

    Add files via upload

---
+++ 08_average_of_numbers_entered_by_user.py
@@ -0,0 +1,4 @@
+a=int(input("Enter the first number to take out average : "))
+b=int(input("Enter the second number to take out average : "))
+sum= a+b
+print("The average of these two is", sum/2 )
---
+++ 09_square_of_a_number_entered_by_the_user.py
@@ -0,0 +1,3 @@
+a=int(input("Enter the number to find the squar : "))
+s= a*a
+print('The square of the given number is', s)
