534545d58e2b47b430d31b459c792bb14b77ac95
Dheeraj Mourya <87568621+Monsterdj07@users.noreply.github.com>
2022-07-04 05:16:54 +0530

    Add files via upload

---
+++ 07_comparing_two_nunbers_given_by_the_user.py
@@ -0,0 +1,7 @@
+a=int(input("Enter the first number to compare: "))
+b=int(input("Enter the second number to compare: "))
+if (a>b):
+	print( a ," is greater than ", b)
+else :
+	print( b ," is greater than ", a)
+ 
