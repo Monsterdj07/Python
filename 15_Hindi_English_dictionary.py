add9c46c3ff8f72b01d3cc4ffa3a1f7ee623e1de
Dheeraj Mourya <87568621+Monsterdj07@users.noreply.github.com>
2022-07-10 22:12:41 +0530

    Add files via upload

---
+++ 15_Hindi_English_dictionary.py
@@ -0,0 +1,3 @@
+a= { "ladka":"boy" , "ladki":"girl", "kutta":"dog", "kutiya":"bitch"}
+b=input("Enter a word from ladka, ladki, kutta , kutiya to know the  English translation of it.\n")
+print(a.get(b))
