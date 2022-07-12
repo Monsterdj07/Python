65c61d78ffe9a2c9f85801f284b41ad617ee66ef
Dheeraj Mourya <87568621+Monsterdj07@users.noreply.github.com>
2022-07-13 00:29:01 +0530

    Add files via upload

---
+++ 24_Enter_value_for_the_keys.py
@@ -0,0 +1,11 @@
+a=input(" George enter a value for your key:\n")
+b=input(" Gokul enter a value for your key:\n")
+c=input(" Shrey enter a value for your key:\n")
+e=input(" Sanal enter a value for your key:\n")
+d={ "george": a, 
+"gokul": b, 
+" shrey":c, 
+"sanal":e }
+print(d)
+
+
