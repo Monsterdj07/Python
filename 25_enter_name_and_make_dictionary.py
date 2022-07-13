6724ad6edb56bc1570cc23fcf70496f6468d4a44
Dheeraj Mourya <87568621+Monsterdj07@users.noreply.github.com>
2022-07-13 23:18:10 +0530

    Add files via upload

---
+++ 25_enter_name_and_make_dictionary.py
@@ -0,0 +1,13 @@
+a=input("Enter your name as a  key:\n")
+b=input("Enter  your favorite language :\n")
+c=input("Enter your name as a key:\n")
+d=input("Enter  your favorite language :\n")
+e=input("Enter your name as akey:\n")
+f=input("Enter  your favorite language:\n")
+g=input("Enter your name as a key:\n")
+h=input("Enter  your favorite language:\n")
+dic={a:b,
+c:d,
+e:f, 
+g:h }
+print(dic) 
